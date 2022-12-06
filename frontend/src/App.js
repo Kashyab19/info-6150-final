4d3a6f0ce8520b37c7122a27d21596352ee58b5c
Srikanth N <srikanth.na@northeastern.edu>
2022-12-05 23:26:51 -0500

    Added sign up ,login and forgot password UI pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import ProductNavigationBar from './components/ProductNavigationBar';
+import Register from './pages/Registration';
+import Login from './pages/Login';
 
 function App() {
   return (
