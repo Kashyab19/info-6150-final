bdafde0e0420c3254d029ee2704aa2cb315daa0c
mrkenkre <mrkenkre@gmail.com>
2022-12-06 16:57:53 -0500

    resolved conflicts from dev/mayur to master branch

---
+++ .history/frontend/src/index_20221206165251.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+import './components/LibraryComp/Library.css';
+import App from './App';
+
+
+
+//External
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.min.css";
+
+//NPMs
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+//Pages
+<<<<<<< HEAD
+import AddProducts from "./pages/AddProducts";
+import { AuthenticationContextProvider } from "./context/AuthenticationContext";
+import Register from "./pages/Registration";
+import Login from "./pages/Login";
+import Verification from "./pages/Verification";
+import ProfilePage from "./pages/Profile";
+=======
+import AddProducts from './pages/AddProducts';
+import RedEye from './pages/RedEye';
+>>>>>>> dev/Mayur
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+<<<<<<< HEAD
+    <AuthenticationContextProvider>
+      <Routes>
+        <Route exact={true} path="/" element={<App />} />
+        <Route exact={true} path="/profile" element={<ProfilePage />} />
+=======
+    <Routes>
+    <Route exact={true} path="/" element={<App />} />
+    <Route exact={true} path="/add-products" element={<AddProducts/>} />
+    <Route exact={true} path="/RedEye" element={<RedEye/>} />
+</Routes>
+</BrowserRouter>
+);
+>>>>>>> dev/Mayur
+
+        <Route exact={true} path="/add-products" element={<AddProducts />} />
+        <Route exact={true} path="/authenticate/*" element={<Login/>} />
+        <Route exact={true} path="/signup" element={<Register/>} />
+        <Route exact={true} path="/verify" element={<Verification/>} />
+        {/* <Route exact={true} path="/signup" element={<Register/>} /> */}
+      </Routes>
+    </AuthenticationContextProvider>
+  </BrowserRouter>
+);
