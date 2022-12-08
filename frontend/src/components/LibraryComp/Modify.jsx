c9ce6b36437d5342cd6cc1f1982e3c2bfbc2318e
Nishaanth-44 <114043295+Nishaanth-44@users.noreply.github.com>
2022-12-08 15:57:18 -0500

    Modify page changes

--- frontend/src/components/LibraryComp/Modify.jsx
+++ frontend/src/components/LibraryComp/Modify.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Row, Col } from "reactstrap";
+import axios from "axios";
 
 
 
@@ -9,14 +10,24 @@ const [loading, setLoading] = useState(true);
 const [error, setError] = useState(null);
 const [word, setWord] = useState();
 
+
 useEffect(() => {
   console.log('Page Loaded');
-  fetch('http://localhost:3001/bookings/')
-  .then((response) => response.json())
-  .then((data) => {
-    setWord(data[0]);
-    console.log(data)
-  })
+  // fetch('http://localhost:3001/bookings/')
+  axios.get('http://localhost:3001/bookings/', {
+    params: { email: 'Reddy'},
+    
+}).then(
+  response => {
+    console.log(response.data)
+    const roomData = response.data
+    setWord(roomData)
+  }
+)
+  // .then((data) => {
+  //   setWord(data[0]);
+  //   console.log(data)
+  // })
   .catch(err => {
       console.log(err);
      });
@@ -28,26 +39,28 @@ useEffect(() => {
       <Row noGutters className="text-center align-items-center">
         <Col>
           <p className="modify-header">Your Bookings</p>
+          {
+            word?.map(e =>{
+              return(
+                <div class="card-booking  card text-bg-secondary mb-3" style= {{"max-width" : "18rem"}}>
+                  <div class="card-header">{e.name}</div>
+                  <div class="card-body">
+                  <h5 class="card-title">{e.location}</h5>
+                  <p>Capacity : {e.capacity}</p>
+                  <p>Booking Date : {e.bookingtime.split("T")[0]}</p>
+                  <p>Booking Time : {e.bookingtime.split("T")[1]}</p>
+                  </div>
+                </div>
+              )
+            })
+          }
         </Col>
       </Row>
-      <Row>
+      {/* <Row>
         <Col className="text-center align-items-center">
-          {/* {word.map((booking) => {
-            return (
-            <p>
-              {booking.name}
-            </p>
-            )
-          })} */}
-          <div class="card-booking  card text-bg-secondary mb-3" style= {{"max-width" : "18rem"}}>
-          <div class="card-header">Header</div>
-          <div class="card-body">
-          <h5 class="card-title">Secondary card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          </div>
-          </div>
+         
         </Col>
-      </Row>
+      </Row> */}
     </div>
   );
 };
