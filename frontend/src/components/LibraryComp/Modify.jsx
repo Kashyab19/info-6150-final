6e4d2621dbe4d5903b813da59d808b2cd184370f
Nishaanth-44 <114043295+Nishaanth-44@users.noreply.github.com>
2022-12-08 15:17:30 -0500

    Library Changes

--- frontend/src/components/LibraryComp/Modify.jsx
+++ frontend/src/components/LibraryComp/Modify.jsx
@@ -7,11 +7,21 @@ export default _ => {
 const [data, setData] = useState(null);
 const [loading, setLoading] = useState(true);
 const [error, setError] = useState(null);
+const [word, setWord] = useState();
 
 useEffect(() => {
-  fetch('http://localhost:3001/bookings')
-   .then((response) => console.log(response));
- }, [data]);
+  console.log('Page Loaded');
+  fetch('http://localhost:3001/bookings/')
+  .then((response) => response.json())
+  .then((data) => {
+    setWord(data[0]);
+    console.log(data)
+  })
+  .catch(err => {
+      console.log(err);
+     });
+  },[]);
+
   return (
     
     <div className="library-main-container vh-100">
@@ -22,6 +32,13 @@ useEffect(() => {
       </Row>
       <Row>
         <Col className="text-center align-items-center">
+          {/* {word.map((booking) => {
+            return (
+            <p>
+              {booking.name}
+            </p>
+            )
+          })} */}
           <div class="card-booking  card text-bg-secondary mb-3" style= {{"max-width" : "18rem"}}>
           <div class="card-header">Header</div>
           <div class="card-body">
